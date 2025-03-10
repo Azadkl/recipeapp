c5db835a9a24224e8fa772a89dc16e3d6015edf1
Azad Köl <kolazad47@gmail.com>
2025-03-10 23:23:15 +0300

    recipescreen ekranı tamamlandı kısmen

--- lib/pages/recipe_screen.dart
+++ lib/pages/recipe_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:recipeapp/widget/widget_support.dart';
 
 class RecipeScreen extends StatefulWidget {
   const RecipeScreen({super.key});
@@ -8,6 +10,7 @@ class RecipeScreen extends StatefulWidget {
 }
 
 class _RecipeScreenState extends State<RecipeScreen> {
+  bool isLiked = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,34 +31,248 @@ class _RecipeScreenState extends State<RecipeScreen> {
                 color: Colors.black87.withOpacity(0.5),
               ),
               child: DraggableScrollableSheet(
-                maxChildSize: 0.7,
+                maxChildSize: 0.8,
                 minChildSize: 0.2,
-                initialChildSize: 0.4,
+                initialChildSize: 0.3,
                 builder: (context, scrollController) {
                   return Container(
+                    padding: EdgeInsets.only(right: 20, left: 20),
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.only(
                         topLeft: Radius.circular(20),
-                        topRight: Radius.circular(20)
+                        topRight: Radius.circular(20),
                       ),
-                      color: Colors.white
+                      color: Colors.white,
                     ),
                     child: SingleChildScrollView(
                       controller: scrollController,
                       child: Column(
                         children: [
                           SizedBox(
-                            width: MediaQuery.of(context).size.width*0.2,
+                            width: MediaQuery.of(context).size.width * 0.2,
                             child: const Divider(
                               color: Colors.black,
                               thickness: 3.0,
                             ),
-                          )
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.04,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                    textAlign: TextAlign.center,
+                                    "Makarna",
+                                    style: AppWidget.semiBoldTextFeildStyle(),
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text(
+                                        maxLines: 2,
+                                        overflow: TextOverflow.ellipsis,
+                                        textAlign: TextAlign.center,
+                                        "Deftere ekle",
+                                        style: AppWidget.LightTextFeildStyle(),
+                                      ),
+                                      SizedBox(
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                            0.02,
+                                      ),
+                                      Icon(Icons.bookmark_outline),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.04,
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    children: [
+                                      CircleAvatar(
+                                        child: Icon(
+                                          Icons.person,
+                                          color: Colors.white,
+                                        ),
+                                        backgroundColor: Colors.black,
+                                      ),
+                                      SizedBox(
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                            0.03,
+                                      ),
+                                      Text(
+                                        "Azad Köl",
+                                        style: Theme.of(
+                                          context,
+                                        ).textTheme.titleMedium?.copyWith(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w700,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      const Icon(
+                                        Icons.favorite,
+                                        color: Colors.red,
+                                      ),
+                                      SizedBox(
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                            0.01,
+                                      ),
+                                      Text(
+                                        "260k beğeni",
+                                        style: Theme.of(
+                                          context,
+                                        ).textTheme.titleMedium?.copyWith(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w700,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.02,
+                              ),
+                              Divider(thickness: 1.0),
+
+                              Text(
+                                "Nasıl Yapılır ?",
+                                style: AppWidget.semiBoldTextFeildStyle(),
+                              ),
+                              for (var items in [
+                                "Bir tencereye suyu koyup kaynatın.",
+                                "Kaynayan suya tuz ve sıvı yağı ekleyin.",
+                                "Makarnayı suya atarak 8-10 dakika haşlayın.",
+                                "Haşlanan makarnayı süzün ve soğuk sudan geçirin.",
+                                "Tereyağını tavada eritip makarnayı ekleyin, karıştırın.",
+                                "Üzerine rendelenmiş peynir ekleyerek servis edin.",
+                              ])
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.local_fire_department,
+                                      color: Colors.orange,
+                                    ),
+
+                                    Text(
+                                      items,
+                                      style: AppWidget.LightTextFeildStyle()
+                                          .copyWith(fontSize: 13),
+                                    ),
+                                  ],
+                                ),
+                              Divider(thickness: 1.0),
+                              Text(
+                                "Tür",
+                                style: AppWidget.semiBoldTextFeildStyle(),
+                              ),
+                              Text(
+                                "Tatlı",
+                                style: AppWidget.LightTextFeildStyle(),
+                              ),
+                              Divider(thickness: 1.0),
+                              Text(
+                                "Yapılış Süresi(dk)",
+                                style: AppWidget.semiBoldTextFeildStyle(),
+                              ),
+                              Text(
+                                "30",
+                                style: AppWidget.LightTextFeildStyle(),
+                              ),
+                              Divider(thickness: 1.0),
+                              Text(
+                                "İçindekiler",
+                                style: AppWidget.semiBoldTextFeildStyle(),
+                              ),
+                              for (var item in [
+                                "1 paket makarna",
+                                "1,5 litre su",
+                                "1 tatlı kaşığı tuz",
+                                "1 yemek kaşığı sıvı yağ",
+                                "Sos için: Tereyağı ve rendelenmiş peynir",
+                              ])
+                                Row(
+                                  children: [
+                                    Icon(Icons.check_box, color: Colors.amber),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        item,
+                                        style: AppWidget.LightTextFeildStyle(),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                            ],
+                          ),
                         ],
                       ),
                     ),
                   );
                 },
+              ),
+            ),
+          ),
+          Positioned(
+            top: 60,
+            left: 10,
+            child: InkWell(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.12,
+                height: MediaQuery.of(context).size.height * 0.06,
+                decoration: BoxDecoration(
+                  color: Color(0xFF61666F).withOpacity(0.8),
+                  borderRadius: BorderRadius.circular(100),
+                  boxShadow: const [
+                    BoxShadow(color: Color(0x0FFFFFFF), blurRadius: 4),
+                  ],
+                ),
+                child: Icon(CupertinoIcons.back),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 60,
+            right: 10,
+            child: InkWell(
+              onTap: () {
+                setState(() {
+                  isLiked = !isLiked;
+                });
+              },
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.12,
+                height: MediaQuery.of(context).size.height * 0.06,
+                decoration: BoxDecoration(
+                  color: Color(0xFF61666F).withOpacity(0.8),
+                  borderRadius: BorderRadius.circular(100),
+                  boxShadow: const [
+                    BoxShadow(color: Color(0x0FFFFFFF), blurRadius: 4),
+                  ],
+                ),
+                child: Icon(isLiked ? Icons.favorite : Icons.favorite_border,color: isLiked ? Colors.red: Colors.black,),
               ),
             ),
           ),
